7f210647042c7681884e87408a8a4afbec869798
Neesons <34045289+Neesons@users.noreply.github.com>
2018-03-26 21:41:39 -0400

    Add files via upload

---
+++ SearchInventoryView.java
@@ -0,0 +1,285 @@
+// specify the package
+package userinterface;
+
+// system imports
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.util.Properties;
+
+// project imports
+import impresario.IModel;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/** The class containing the Modify Clothing View  for the Professional Clothes
+ *  Closet application 
+ */
+//==============================================================
+public class SearchInventoryView extends View
+{
+
+	// GUI components
+	protected TextField barcode;
+
+	protected Button submitButton;
+	protected Button cancelButton;
+
+	// For showing error message
+	protected MessageView statusLog;
+
+	// constructor for this class -- takes a model object
+	//----------------------------------------------------------
+	public SearchInventoryView(IModel ic)
+	{
+		super(ic, "SearchInventoryView");
+		// create a container for showing the contents
+		VBox container = new VBox(10);
+                container.setStyle("-fx-background-color: slategrey");
+		container.setPadding(new Insets(15, 5, 5, 5));
+		// Add a title for this panel
+		container.getChildren().add(createTitle());
+		// create our GUI components, add them to this Container
+		container.getChildren().add(createFormContent());
+		container.getChildren().add(createStatusLog("             "));
+
+		getChildren().add(container);
+
+		//populateFields();
+
+		myModel.subscribe("TransactionError", this);
+	}
+
+	//-------------------------------------------------------------
+	protected String getActionText()
+	{
+		return "** SEARCH FOR CLOTHING ITEM **";
+	}
+
+	// Create the title container
+	//-------------------------------------------------------------
+	private Node createTitle()
+	{
+		VBox container = new VBox(10);
+		container.setPadding(new Insets(1, 1, 1, 30));
+		
+		Text clientText = new Text("OFFICE OF CAREER SERVICES");
+		clientText.setFont(Font.font("Copperplate", FontWeight.EXTRA_BOLD, 25));
+		clientText.setWrappingWidth(350);
+		clientText.setTextAlignment(TextAlignment.CENTER);
+		clientText.setFill(Color.DARKGREEN);
+		container.getChildren().add(clientText);
+		
+		Text titleText = new Text(" Professional Clothes Closet Management System ");
+		titleText.setFont(Font.font("Comic Sans", FontWeight.THIN, 25));
+		titleText.setWrappingWidth(350);
+		titleText.setTextAlignment(TextAlignment.CENTER);
+		titleText.setFill(Color.GOLD);
+		container.getChildren().add(titleText);
+
+		Text blankText = new Text("  ");
+		blankText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		blankText.setWrappingWidth(350);
+		blankText.setTextAlignment(TextAlignment.CENTER);
+		blankText.setFill(Color.WHITE);
+		container.getChildren().add(blankText);
+
+		Text actionText = new Text("   " + getActionText() + "     ");
+		actionText.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+		actionText.setWrappingWidth(350);
+		actionText.setTextAlignment(TextAlignment.CENTER);
+		actionText.setFill(Color.CORAL);
+		container.getChildren().add(actionText);
+	
+		return container;
+	}
+
+	// Create the main form content
+	//-------------------------------------------------------------
+	private VBox createFormContent()
+	{
+		VBox vbox = new VBox(10);
+		
+                Text blankText = new Text("  ");
+		blankText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		blankText.setWrappingWidth(350);
+		blankText.setTextAlignment(TextAlignment.CENTER);
+		blankText.setFill(Color.WHITE);
+		vbox.getChildren().add(blankText);
+                
+		Text prompt1 = new Text("Enter Clothing Item Barcode:");
+                prompt1.setWrappingWidth(400);
+                prompt1.setTextAlignment(TextAlignment.CENTER);
+                prompt1.setFill(Color.BLACK);
+		prompt1.setFont(Font.font("Copperplate", FontWeight.SEMI_BOLD, 18));
+		vbox.getChildren().add(prompt1);
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+                grid.setHgap(10);
+                grid.setVgap(10);
+                grid.setPadding(new Insets(0, 25, 10, 0));
+		
+		Text barcodeLabel = new Text(" Clothing Barcode : ");
+		Font myFont = Font.font("Comic Sans", FontWeight.THIN, 14);
+		barcodeLabel.setFont(myFont);
+                barcodeLabel.setFill(Color.GOLD);
+		barcodeLabel.setWrappingWidth(150);
+		barcodeLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(barcodeLabel, 0, 1);
+
+		barcode = new TextField();
+                barcode.setStyle("-fx-focus-color: darkgreen;");
+		grid.add(barcode, 1, 1);
+		
+		vbox.getChildren().add(grid);
+
+		HBox doneCont = new HBox(10);
+		doneCont.setAlignment(Pos.CENTER);
+                ImageView icon = new ImageView(new Image("/images/searchcolor.png"));
+                icon.setFitHeight(15);
+                icon.setFitWidth(15);
+		submitButton = new Button("Search",icon);
+                submitButton.setStyle("-fx-background-color: lightgreen; ");
+		submitButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+		submitButton.setOnAction((ActionEvent e) -> {
+                    clearErrorMessage();
+                    Properties props = new Properties();
+                    String bc = barcode.getText();
+                    if (bc.length() > 0)
+                    {
+                        props.setProperty("Barcode", bc);
+                        myModel.stateChangeRequest("ClothingItemSelected", props);
+                    }
+                    else
+                        displayErrorMessage("ERROR: Please Enter A Barcode To Search On!");
+                });
+                submitButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+                    submitButton.setEffect(new DropShadow());
+                });
+                submitButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+                    submitButton.setEffect(null);
+                });
+		doneCont.getChildren().add(submitButton);
+		icon = new ImageView(new Image("/images/return.png"));
+                icon.setFitHeight(15);
+                icon.setFitWidth(15);
+		cancelButton = new Button("Return",icon);
+                cancelButton.setStyle("-fx-background-color: PALEVIOLETRED; ");
+		cancelButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+		cancelButton.setOnAction((ActionEvent e) -> {
+                    clearErrorMessage();
+                    myModel.stateChangeRequest("CancelSearchInventory", null);
+                });
+                cancelButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+                    cancelButton.setEffect(new DropShadow());
+                });
+                cancelButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+                    cancelButton.setEffect(null);
+                });
+		doneCont.getChildren().add(cancelButton);
+                blankText = new Text("  ");
+		blankText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		blankText.setWrappingWidth(350);
+		blankText.setTextAlignment(TextAlignment.CENTER);
+		blankText.setFill(Color.WHITE);
+		vbox.getChildren().add(blankText);
+		vbox.getChildren().add(doneCont);
+		return vbox;
+	}
+
+
+	// Create the status log field
+	//-------------------------------------------------------------
+	protected MessageView createStatusLog(String initialMessage)
+	{
+		statusLog = new MessageView(initialMessage);
+
+		return statusLog;
+	}
+	
+
+
+	//-------------------------------------------------------------
+	public void populateFields()
+	{
+		
+	}
+
+	/**
+	 * Update method
+	 */
+	//---------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		clearErrorMessage();
+
+		if (key.equals("TransactionError") == true)
+		{
+			String val = (String)value;
+			if (val.startsWith("ERR") == true)
+			{
+				displayErrorMessage(val);
+			}
+			else
+			{
+				displayMessage(val);
+			}
+			
+		}
+	}
+
+	/**
+	 * Display error message
+	 */
+	//----------------------------------------------------------
+	public void displayErrorMessage(String message)
+	{
+		statusLog.displayErrorMessage(message);
+	}
+
+	/**
+	 * Display info message
+	 */
+	//----------------------------------------------------------
+	public void displayMessage(String message)
+	{
+		statusLog.displayMessage(message);
+	}
+
+	/**
+	 * Clear error message
+	 */
+	//----------------------------------------------------------
+	public void clearErrorMessage()
+	{
+		statusLog.clearErrorMessage();
+	}
+
+}
+
+//---------------------------------------------------------------
+//	Revision History:
+//
+
+
