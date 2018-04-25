dcc862bfb150c71423ccb04b5e29936658f4f3ba
Writ3r <lucas.wing@aol.com>
2018-04-25 15:17:52 -0400

    Add files via upload

--- userinterface/EnterRecepientInfoView.java
+++ userinterface/EnterRecepientInfoView.java
@@ -203,6 +203,13 @@ public class EnterRecepientInfoView extends View {
 		icon.setFitWidth(15);
 		submitButton = new Button("Add", icon);
 		submitButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+		//checks to make sure we actually have an inventory item
+		String barcode = (String)myModel.getState("BarcodeCheck");
+		if (barcode != null)
+		{
+			Integer.parseInt(barcode);
+		}
+		
 		submitButton.setOnAction((ActionEvent e) -> {
 			clearOutlines();
 			clearErrorMessage();
